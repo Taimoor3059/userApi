1f88c55b8eab150ffe4057bfb818120d657fe6c4
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-03-13 16:30:47 -0500

    fetching the data

--- App.js
+++ App.js
@@ -3,7 +3,55 @@ import { StyleSheet, Text, View, FlatList, Image, ActivityIndicator } from 'reac
 import { Card } from 'native-base';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: []
+    }
+  }
+
+  getUserFromApi = () => {
+    return(
+      fetch('https://randomuser.me/api/?results=50')
+        .then(response => response.json())
+        .then(responseJson => {
+          this.setState({
+            isLoading: false,
+            dataSource: this.state.dataSource.concat(responseJson.results)
+          })
+          console.log(this.state.dataSource)
+        })
+        
+        .catch(error => {
+          console.log(error)
+        })
+    )
+  }
+
+  _keyExtractor = (datasource, index) => dataSource.email;
+
+
+
+  componentDidMount() {
+    this.getUserFromApi();
+    
+  }
+  
+
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <FlatList
+        data= {this.state.dataSource}
+        keyExtractor={this._keyExtractor}
+        renderItem={ (item) => (
+          <Card></Card>
+        )  } 
+        ></FlatList>
+      );
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start  hello working on your app!</Text>
